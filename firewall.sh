87acd3af605ed3050901769e8d5b56c20fc75e42
tgonzales114 <thomasgonzales114@gmail.com>
2022-12-18 16:35:45 -0500

    add logic to firewall.sh to check if firewalld is running

--- firewall.sh
+++ firewall.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-firewall-cmd --add-port=5000/tcp --zone=internal --permanent
-firewall-cmd --add-port=5000/tcp --zone=public --permanent
-firewall-cmd --reload
+echo "checking if firewalld is running..."
+if systemctl -q is-active firewalld; then
+    echo "firewalld is running, opening port 5000"
+    firewall-cmd --add-port=5000/tcp --zone=internal --permanent
+    firewall-cmd --add-port=5000/tcp --zone=public --permanent
+    firewall-cmd --reload
+else
+    echo "firewalld is not running, quiting"
+    exit 1
+fi
